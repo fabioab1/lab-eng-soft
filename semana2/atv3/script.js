b6b4c7af96ba499d42c48ae853cecefe8576c61e
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2025-02-23 13:17:03 -0300

    Atividade 3 concluída.

---
+++ semana2/atv3/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    function inverterTexto(textoOriginal) {
+        let textoAoContrario = "";
+        for (let i=textoOriginal.length-1; i>=0; i--)
+            textoAoContrario += textoOriginal.charAt(i); 
+        return textoAoContrario;
+    }
+
+    document.getElementById("btn").addEventListener("click", function() {
+
+        let input = document.getElementById("texto");
+        let texto = input.value.replace(/\s/g, '');
+        let textoAoContrario = inverterTexto(texto);
+
+        console.log(texto);
+        console.log(textoAoContrario);
+
+        if (texto == textoAoContrario)
+            alert("A frase/palavra é um palíndromo.");
+        else
+            alert("O frase/palavra não é um palíndromo.");
+
+    })
+
+})
